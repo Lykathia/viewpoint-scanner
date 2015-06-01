625e1a20b12d33d9697aefecdb23337ac6624eb7
Evan Lowry <lowry.e@gmail.com>
2015-06-01 20:59:35 -0300

    Initial Commit
    Exports CSV data using ViewPoint's API.

---
+++ README.md
@@ -0,0 +1,10 @@
+ViewPoint Scanner
+=================
+
+Demo purposes only. Exports CSV data from ViewPoint API. To be used in 
+conjunction with spreadsheets or something.
+
+License
+=======
+
+The code in this repository is licensed under [GPL version 2](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html).
---
+++ scanner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Scanner for consolidating information from ViewPoint.
+"""
+
+import json
+from urllib import request
+import sys
+
+VIEWPOINT_API = 'http://www.viewpoint.ca/api/v1/properties/'
+
+
+def get_viewpoint_json(pid):
+    """
+    e.g. http://www.viewpoint.ca/api/v1/properties/40548778.json
+    """
+    response = request.urlopen(VIEWPOINT_API + pid + '.json')
+    return json.loads(response.readall().decode('utf-8'))
+
+
+def filter_keys(dct):
+    """ Filter to just be keys we're interested in. """
+    keys = ['list_price', 'assessment', 'listdate', 'area', 'age']
+    tmp = [dct[k] for k in keys]
+    tmp.append(dct['address']['civicnum'] + ' ' + dct['address']['street'])
+    tmp.append(dct['address']['city'])
+    return tmp
+
+
+def to_csv(prop):
+    """ Convert response to csv """
+    return ','.join(filter_keys(prop))
+
+
+def main():
+    """ Init Runner """
+    for pid in sys.argv[1:]:
+        print(to_csv(get_viewpoint_json(pid)))
+
+
+if __name__ == '__main__':
+    main()
